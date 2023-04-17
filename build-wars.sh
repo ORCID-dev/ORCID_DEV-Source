98d3aedd700755430a0f59b89af7d204e8bc5978
Giles Westwood <giles.westwood@gmail.com>
2023-04-17 18:50:46 +0100

    refactor: use proper shellkit checkout and aws secrets

--- build-wars.sh
+++ build-wars.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
-which mvn
-current_dir=`pwd`
-
+# exit on errors
 set -o errexit -o errtrace -o nounset -o functrace -o pipefail
 shopt -s inherit_errexit 2>/dev/null || true
+trap 'sk-catch --exit_code $? --line $LINENO --linecallfunc "$BASH_COMMAND" --funcstack $(printf "::%s" ${FUNCNAME[@]}) -o stdout ' ERR
 
-trap 'echo "exit_code $? line $LINENO linecallfunc $BASH_COMMAND"' ERR
+# import shellkit functions
+source shellkit_bootstrap.sh
+
+# defaults
+current_dir=`pwd`
+checkout_name=$(basename `pwd`)
+NAME="$(basename "${0}")"
+build_envs="prod sandbox qa int"
+tag="v2.0.1"
+current_dir=`pwd`
+
+#
+# functions
+#
 
 reset_poms(){
   git checkout orcid-activemq/pom.xml
@@ -25,30 +37,83 @@ reset_poms(){
 
 }
 
-. ~/work/shellkit/profile.d/shellkit.sh
 
-############################################################
-if [[ ! -d ~/.asdf/plugins/mvnd/ ]];then
-  asdf plugin-add mvnd https://github.com/joschi/asdf-mvnd
-fi
+usage(){
+I_USAGE="
+
+  Usage: ${NAME} [OPTIONS]
+
+  Description:
+
+    Build orcid-source war files for each project and then deploy to and artifact repo
+
+    NOTE: credentials for the artifact repo are sourced from aws secrets but you still need your aws api access to be configured
+          secretid is stored in shellkit.conf
+
+  General usage:
+
+    ${NAME} -t vx.x.x
+
+  Required options:
+      -t | --tag         )  tag ($tag)
+
+"
+  echo "$I_USAGE"
+  exit
+
+}
+
+#
+# args
+#
+
+while :
+do
+  case ${1-default} in
+      --*help|-h         )  usage ; exit 0 ;;
+      -t | --tag         )  tag=$2; shift 2 ;;
+      -b | --build_envs   )  build_envs=$2; shift 2 ;;
+      -v | --verbose )       verbose_arg='-v' VERBOSE=$((VERBOSE+1)); shift ;;
+      --) shift ; break ;;
+      -*) echo "WARN: Unknown option (ignored): $1" >&2 ; shift ;;
+      *)  break ;;
+    esac
+done
+
+sk-arg-check tag
+
+tag_numeric=$(echo "$tag" | tr -dc '[:digit:].')
+echo_log "building for: $tag_numeric"
+
+reset_poms
+
+mvn clean
+rm -Rf ~/.m2/orcid-source-repo/org/orcid
+
+#
+# setup build environment from .tool-versions
+#
+echo_log "configure build environment for orcid-angular $tag_numeric"
 
 sk-asdf-install-tool-versions
 # set JAVA_HOME
 . ~/.asdf/plugins/java/set-java-home.bash
 _asdf_java_update_java_home
-############################################################
 
+sk-dir-make ~/log
 
-tag=${1:-v2.0.1}
-tag_numeric=$(echo "$tag" | tr -dc '[:digit:].')
+echo $AWS_SECRET_ID
+# source the secrets for the artifact uploads
+sk-aws-secret-source $AWS_SECRET_ID
 
-reset_poms
+echo ${ARTIFACT_URL}${ARTIFACT_REPO_PATH}
 
-mvn clean
-# rm -Rf ~/.m2/repository/org/orcid/orcid-test
-rm -Rf ~/.m2/repository/org/orcid
+export ARTIFACT_USER=$ARTIFACT_USER
+export ARTIFACT_PASSWORD=$ARTIFACT_PASSWORD
 
-##########
+#
+# build and version bump
+#
 
 # bump the tagged version in the poms tied to the parent pom
 mvn versions:set -DnewVersion=$tag_numeric -DgenerateBackupPoms=false
