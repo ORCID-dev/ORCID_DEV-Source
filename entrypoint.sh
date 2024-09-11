93438e66bb03755cf003afa6897a106523465e16
Giles Westwood <giles.westwood@gmail.com>
2024-09-11 12:39:41 +0100

    basic properties file templating to try and get orcid-web/tomcatUp.json working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+cd /usr/local/tomcat
+
+# template any properties files
+for j2_file in *.j2;do
+  j2 $j2_file -o $(basename $j2_file .j2) --undefined
+done
+
 /usr/local/tomcat/bin/catalina.sh run
